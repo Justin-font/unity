9f8606366018f869b4904aa053617fe3d8217be9
font justin <fontjustin@gmail.com>
2020-01-22 14:12:26 +0100

    first commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Main : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+    static Main _instance;
+    
+    public static Main Instance
+    {
+        get
+        {
+            if(_instance == null){
+                _instance = GameObject.FindObjectOfType<Main>();
+            }
+            return _instance;
+        }
+    }
+
+    public void GameOver()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+//        scoreManager.Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LaunchGame()
+    {
+        print("launch game ");
+    }
+
+}
