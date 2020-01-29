facf95566a5174ef2574a323925be67d479ebc45
jusitn <fontjustin@gmail.com>
2020-01-29 16:51:52 +0100

    scroll et bouton

---
+++ Assets/Scripts/InstantiationExample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InstantiationExample : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject goButton = (GameObject)Instantiate(myPrefab);
+        goButton.transform.SetParent(GameObject.Find("Grid").transform, false);
+        goButton.transform.localScale = new Vector3(1, 1, 1);
+
+        Button tempButton = goButton.GetComponent<Button>();
+        int tempInt = i;
+        tempButton.onClick.AddListener(() => ButtonClicked(tempInt));
+           goButton.GetComponentInChildren<Text>().text = i.ToString();
+        }
+    }
+    void ButtonClicked(int buttonNo)
+    {
+        Debug.Log("Button clicked = " + buttonNo);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
