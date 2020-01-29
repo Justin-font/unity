a7b441788ba0d65349ca5d4c21cafcae12911406
jusitn <fontjustin@gmail.com>
2020-01-29 13:51:16 +0100

    bouton start et menu start

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class Main : MonoBehaviour
 {
     public ScoreManager scoreManager;
@@ -38,6 +39,23 @@ public class Main : MonoBehaviour
     public void LaunchGame()
     {
         print("launch game ");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
+
+    public void StartGame()
+    {
+        print("start game ");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+
 
 }
