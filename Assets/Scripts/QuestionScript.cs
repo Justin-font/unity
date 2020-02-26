3a14bde63a4f6b0bbbb193752787b76e814c403e
poirel_leo <leo.poirel@supelec.fr>
2020-02-26 16:58:20 +0100

    Animation updates et arrêt du perso à chaque porte

--- Assets/Scripts/QuestionScript.cs
+++ Assets/Scripts/QuestionScript.cs
@@ -8,6 +8,7 @@ public class QuestionScript : MonoBehaviour
     public List<Text> leftAnswers;
     public List<Text> questions;
     public List<Text> rightAnswers;
+    public static List<bool> leftAnswerIsTrue;
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +17,25 @@ public class QuestionScript : MonoBehaviour
 
         Questions load_questions = Main.readJSON("Assets/data/_game 1_.txt");
 
+        leftAnswerIsTrue = new List<bool>();
+
         foreach (Question question in load_questions.questions)
         {
             if (index < leftAnswers.Count){
                 if (Random.Range(0, 1) == 0){
                     leftAnswers[index].text = question.trueResponse;
                     rightAnswers[index].text = question.falseResponse;
+                    leftAnswerIsTrue.Add(true);
                 }
                 else{
                     leftAnswers[index].text = question.falseResponse;
                     rightAnswers[index].text = question.trueResponse;
+                    leftAnswerIsTrue.Add(false);
                 }
                 questions[index].text = question.question;
             }
+
+            index += 1;
         }
     }
 
--- Assets/Scripts/RightDoorController.cs
+++ Assets/Scripts/RightDoorController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class RightDoorController : MonoBehaviour
 {
+    private Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("openRight", true);
+
+            PlayerController pc = other.gameObject.GetComponent(typeof(PlayerController)) as PlayerController;
+
+            if(!QuestionScript.leftAnswerIsTrue[pc.step]){
+                print("Bien joué !");
+            }
+            else{
+                print("Mauvaise réponse...");
+            }
+            pc.step += 1;
+        }
     }
 }
